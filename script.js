a516aa61f60b325885540073a8fca66198510e8c
shivani sharma <134792930+shivanisharma33@users.noreply.github.com>
2024-08-10 17:43:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function data(){
+
+
+var a=document.getElementById("inputName").value;
+var b=document.getElementById("inputPassword").value;
+if(a==""||b==""){
+    alert("All field are medatory");
+    return false
+}
+else if(a==a||b==b){
+    alert("submit sucessfully!");
+}
+}
+
+function fun(){
+    confirm("dixitshivi@gmail.com")
+
+}
